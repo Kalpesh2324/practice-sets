d732b3f6b7a6324c4c7c71995f90953b255b08de
Kalpesh2324 <kalpeshdubey23@gmail.com>
2025-09-12 00:12:21 +0530

    Add files via upload

---
+++ practiceSet1.js
@@ -0,0 +1,33 @@
+/* Practice Set 1 — Console Basics
+Covers:
+- Printing messages in the console
+- Using single-line and multi-line comments
+- Performing arithmetic operations
+- Writing a simple greeting program
+Goal: Understand how to use the JavaScript console effectively. */
+
+
+/*1. Print your name in the console.
+2. Print 5 different messages in the console, each on a new line.
+3. Add single-line and multi-line comments to your code.
+4. Print the result of 10 + 15 using console.log().
+5. Create a small program that prints a greeting with your name.*/
+
+
+// solutions:-
+
+/* 1. console.log("kalpesh"); */
+
+/* 2. console.log("This is the first message.");
+console.log("This is the second message.");
+console.log("This is the third message.");
+console.log("This is the fourth message. ");
+console.log("This is the fifth message."); */
+
+//3. This is single line comment.
+
+/* This is mutlti line comment */
+
+// 4. console.log(10+15);
+
+// 5. console.log("Good morning! Kalpesh.");
