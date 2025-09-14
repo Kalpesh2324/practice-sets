deea746fb499fa037fba6cf2847a0e2790f5dd31
Kalpesh2324 <kalpeshdubey23@gmail.com>
2025-09-14 23:57:05 +0530

    Add files via upload

---
+++ practiceSet4.js
@@ -0,0 +1,57 @@
+/* Practice Set 4 — Operators
+Covers:
+- Arithmetic operators (+, -, *, /, %)
+- Comparison operators (==, ===, !=, >, <, etc.)
+- Logical operators (&&, ||, !)
+- Assignment operators (+=, -=, *=, /=)
+Goal: Practice performing calculations, comparisons, and logic in JavaScript. */
+
+/* 
+Questions:-
+
+1. Perform addition, subtraction, multiplication, and division with two variables.
+2. Use modulus (%) to find remainder of 17 ÷ 5.
+3. Compare two variables using == and === and print results.
+4. Use logical operators with boolean variables and print results.
+5. Demonstrate assignment operators (+=, *=) on a variable and print final value. 
+*/
+
+
+/* 1.
+let x = 10;
+let y = 2;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+*/
+
+
+/* 2.
+console.log(17 % 5); //2
+*/
+
+
+/* 3.
+let a = 5;
+let b = "5";
+console.log(a == b); // true 
+console.log(a === b); // false
+ */
+
+
+/* 4.
+console.log(true && false);
+console.log(false || false);
+console.log(!false);
+*/
+
+/* 
+let a = 15;
+let b = 5;
+console.log(a += b);
+console.log(a -= b);
+console.log(a *= b);
+console.log(a /= b);
+console.log(a %= b);
+ */
