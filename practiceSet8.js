8daca028f9bd9b5ca6b4ae273b3bf31e21d0fe90
kalpesh2324 <kalpeshdubey23@gmail.com>
2025-09-18 23:52:32 +0530

    Arrays

---
+++ practiceSet8.js
@@ -0,0 +1,20 @@
+/* Practice Set 8 — Arrays
+Covers:
+- Creating and modifying arrays
+- Accessing elements with indexes
+- Common methods (push, pop, shift, unshift, splice, slice, indexOf)
+- Iterating arrays with for, for...of, and forEach
+Goal: Learn how to store, update, and process collections of data in arrays. */
+
+// Questions:-
+
+
+/* 
+1. Create an array of your favorite foods and print it.
+2. Replace the second item in the array with another food.
+3. Add a new food at the end and at the beginning of the array.
+4. Remove the first and last items from the array.
+5. Find the index of a specific food.
+6. Extract a subarray of 2 elements from the array.
+7. Loop through the array and print each food item using for and for...of.
+ */
