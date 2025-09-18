8d3f00b069cf84e46cfaf1703ba8a7b36ebcff3f
kalpesh2324 <kalpeshdubey23@gmail.com>
2025-09-19 01:40:54 +0530

    Updated Arrays practice set

--- practiceSet8.js
+++ practiceSet8.js
@@ -18,3 +18,71 @@ Goal: Learn how to store, update, and process collections of data in arrays. */
 6. Extract a subarray of 2 elements from the array.
 7. Loop through the array and print each food item using for and for...of.
  */
+
+
+/* 
+//1. 
+const arr = ["Cake", "Dal-chawal","paneer ki bhaji"," Baaki mujhe bhi nahi pata"];
+console.log(arr);
+ */
+
+/* 
+//2. 
+const arr = ["Cake", "Dal-chawal","paneer ki bhaji"," Baaki mujhe bhi nahi pata"];
+arr[1] = "veg-Biryani";
+console.log(arr);
+ */
+
+/* 
+//3.
+const arr = ["Cake", "Dal-chawal","paneer ki bhaji"," Baaki mujhe bhi nahi pata"];
+arr.push("pulav");
+arr.unshift("aloo ke parathe");
+console.log(arr);
+ */
+
+/* 
+//4.
+const arr = ["Cake", "Dal-chawal", "paneer ki bhaji", " Baaki mujhe bhi nahi pata"];
+arr.shift();
+arr.pop();
+console.log(arr);
+ */
+
+
+/* 
+// 5. 
+const arr = ["Cake", "Dal-chawal", "paneer ki bhaji", "Baaki mujhe bhi nahi pata"];
+console.log(arr.indexOf("Baaki mujhe bhi nahi pata"));
+ */
+/* 
+//6.
+const newArr = ["a","b","c","d","e","f"];
+console.log(newArr.slice(1,3));
+
+ */
+/* 
+//7.
+
+// Using for loop:- 
+
+const newArr = ["apple","ball","cat","dog","elephant","fish"];
+for(let a = 0; a < newArr.length-1;a++){
+    console.log(newArr[a]);
+}
+
+//OR//
+
+// Using for...of loop:-
+const newArr = ["apple","ball","cat","dog","elephant","fish"];
+for( let names of newArr){
+    console.log(names);
+}
+
+//Or//
+
+// Using forEach loop:- 
+
+const newArr = ["apple","ball","cat","dog","elephant","fish"];
+newArr.forEach(names=>{console.log(names)});
+*/
